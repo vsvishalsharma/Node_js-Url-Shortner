95801eb70a70097bc40d472b866f9d163e2968cd
vsvishal <vsvishalsharma777@gmail.com>
2023-11-03 16:20:49 +0530

    used Express to effeciently bulid a server

--- My_First_Node_Server/index.js
+++ My_First_Node_Server/index.js
@@ -1,25 +1,9 @@
-const http=require("http");
-const fs=require("fs");
-const url=require("url");
-const my_server=http.createServer((req,res)=>{
-    const log=`${Date.now()}: '${req.url} New Request Received\n`;
-    fs.appendFile("log.txt",log,(err,data)=>{
-        if(!err){
-            const myurl=url.parse(req.url,true);
-            console.log(myurl);
-            switch(myurl.pathname){
-                case"/":
-                    res.end("Welcome to HomePage");
-                    break;
-                case"/about":
-                    const user=myurl.query.username;
-                    res.end(`Hello ${user}`);
-                    break;
-                default:
-                    res.end("404 Not Found");
-                    break;   
-            }
-    }
-    })
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{//phle request aati h phir response
+    return res.send("Hello You Are At HomePage");
 })
-my_server.listen(8000,()=>console.log("Server Started"));
+app.get('/about',(req,res)=>{
+    return res.send('You reached the about page');
+})
+app.listen(8000,()=>console.log("server started"));
