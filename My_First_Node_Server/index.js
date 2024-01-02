8410a272384fe1c353ecf291345bbde093fed9e6
vsvishal <vsvishalsharma777@gmail.com>
2024-01-02 12:24:31 +0530

    removing extra baggage

--- My_First_Node_Server/index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{//phle request aati h phir response
-    return res.send("Hello You Are At HomePage");
-})
-app.get('/about',(req,res)=>{
-    return res.send('You reached the about page');
-})
-app.listen(8000,()=>console.log("server started"));
